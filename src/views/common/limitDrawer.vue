fb5abee14ced6a88a17163b99261342b79e25e9e
xin.xiong <1937565945@qq.com>
2023-07-26 16:24:42 +0800

    feat(首页) 编写

---
+++ src/views/common/limitDrawer.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-drawer
+    v-model="limitDialog"
+    title="标题"
+    direction="rtl"
+    size="50%"
+  >
+    
+  </el-drawer>
+</template>
+  
+<script lang="ts">
+import { defineComponent, onMounted, reactive, ref, toRefs } from "vue";
+
+export default defineComponent({
+  name: 'limitDrawer',
+  components: {
+    
+  },
+  setup() {
+    const data = reactive({
+      limitDialog:false,
+    })
+    const open = () => {
+      data.limitDialog = true
+    }
+    return {
+      ...toRefs(data),
+      open
+    }
+  },
+})
+</script>
+  
+<style lang="scss" scoped>
+</style>
+  
---
+++ src/views/common/settingIcon.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-tooltip
+    class="box-item"
+    effect="dark"
+    content="Bottom Center prompts info"
+    placement="bottom"
+  >
+    <el-icon class="btnIcon" @click="btnSetting"><Setting /></el-icon>
+  </el-tooltip>
+  <!-- 组件 -->
+  <limitDrawer ref="limitDrawerRef"></limitDrawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, reactive, ref, toRefs } from "vue";
+import { Setting } from "@element-plus/icons-vue";
+import limitDrawer from './limitDrawer.vue'
+
+export default defineComponent({
+  name: 'settingIcon',
+  emits: ['reload'],
+  components: {
+    Setting,
+    limitDrawer
+  },
+  props: {
+    value: {
+      type: String
+    },
+  },
+  setup(props, {  emit }) {
+    const limitDrawerRef = ref();
+    const data = reactive({
+    })
+    const btnSetting = () => {
+      limitDrawerRef.value.open()
+    }
+    return {
+    ...toRefs(data),
+    limitDrawerRef,
+    btnSetting
+    }
+  },
+})
+</script>
+    
+<style lang="scss" scoped>
+</style>
+    
