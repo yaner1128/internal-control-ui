94bc7360b6c9fa59a9ab19ff1e07d0e75fe8eec4
liyanyan <1831163246@qq.com>
2023-08-08 10:45:11 +0800

    feat(组件): 修改表头

--- src/component/settingVue/limitDrawer.vue
+++ src/component/settingVue/limitDrawer.vue
@@ -6,7 +6,7 @@
     size="60%"
   >
     <div class="title">模块名称：</div>
-    <div class="checkClass">
+    <!-- <div class="checkClass">
       <el-checkbox v-model="checkList.check1" label="浏览" size="large" @change="checkChange(checkList.check1,'check1')"/>
       <el-checkbox v-model="checkList.check2" label="选择" size="large" @change="checkChange(checkList.check2,'check2')"/>
       <el-checkbox v-model="checkList.check3" label="新增" size="large" @change="checkChange(checkList.check3,'check3')"/>
@@ -16,7 +16,7 @@
       <el-checkbox v-model="checkList.check7" label="配置" size="large" @change="checkChange(checkList.check7,'check7')"/>
       <el-checkbox v-model="checkList.check8" label="执行" size="large" @change="checkChange(checkList.check8,'check8')"/>
       <el-checkbox v-model="checkList.checkAll" label="全选" size="large" @change="checkChange(checkList.checkAll,'checkAll')"/>
-    </div>
+    </div> -->
     <vxe-table
       border
       height="730"
@@ -26,6 +26,9 @@
       :checkbox-config="{highlight: true, range: true}">
       <vxe-column field="name"></vxe-column>
       <vxe-column title="浏览" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check1" label="浏览" size="large" @change="checkChange(checkList.check1,'check1')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check1" ></vxe-checkbox>
         </template>
@@ -34,6 +37,9 @@
         </template>
       </vxe-column>
       <vxe-column title="选择" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check2" label="选择" size="large" @change="checkChange(checkList.check2,'check2')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check2" ></vxe-checkbox>
         </template>
@@ -42,6 +48,9 @@
         </template>
       </vxe-column>
       <vxe-column title="新增" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check3" label="新增" size="large" @change="checkChange(checkList.check3,'check3')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check3" ></vxe-checkbox>
         </template>
@@ -50,6 +59,9 @@
         </template>
       </vxe-column>
       <vxe-column title="编辑" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check4" label="编辑" size="large" @change="checkChange(checkList.check4,'check4')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check4" ></vxe-checkbox>
         </template>
@@ -58,6 +70,9 @@
         </template>
       </vxe-column>
       <vxe-column title="删除" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check5" label="删除" size="large" @change="checkChange(checkList.check5,'check5')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check5" ></vxe-checkbox>
         </template>
@@ -66,6 +81,9 @@
         </template>
       </vxe-column>
       <vxe-column title="导入/出" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check6" label="导入/出" size="large" @change="checkChange(checkList.check6,'check6')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check6" ></vxe-checkbox>
         </template>
@@ -74,6 +92,9 @@
         </template>
       </vxe-column>
       <vxe-column title="配置" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check7" label="配置" size="large" @change="checkChange(checkList.check7,'check7')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check7" ></vxe-checkbox>
         </template>
@@ -82,6 +103,9 @@
         </template>
       </vxe-column>
       <vxe-column title="执行" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.check8" label="执行" size="large" @change="checkChange(checkList.check8,'check8')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.check8" ></vxe-checkbox>
         </template>
@@ -90,6 +114,9 @@
         </template>
       </vxe-column>
       <vxe-column title="全选" :edit-render="{}" show-overflow>
+        <template #header>
+          <el-checkbox v-model="checkList.checkAll" label="全选" size="large" @change="checkChange(checkList.checkAll,'checkAll')"/>
+        </template>
         <template #default="{ row }">
           <vxe-checkbox v-model="row.checkAll" ></vxe-checkbox>
         </template>
@@ -99,8 +126,8 @@
       </vxe-column>
     </vxe-table>
     <div class="drawer__footer">
-      <el-button>Cancel</el-button>
-      <el-button type="primary">Submit</el-button>
+      <el-button>取 消</el-button>
+      <el-button type="primary">确 认</el-button>
     </div>
   </el-drawer>
 </template>
